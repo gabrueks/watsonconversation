92170f74c44bc459f15f61776fe80a82e0ac903b
Gabriel Bolzi <gabriel03899@hotmail.com>
2018-04-11 14:29:34 -0300

    done

---
+++ credential.js
@@ -0,0 +1,14 @@
+var config = {
+    watson: {
+        conversation: {
+            username: "Assistant username",
+            password: "Assistant password",
+            version: "v1",
+            version_date: "2017-05-26",
+            workspace_id: "workspace id"
+         }
+    }
+};
+
+
+module.exports = config;
